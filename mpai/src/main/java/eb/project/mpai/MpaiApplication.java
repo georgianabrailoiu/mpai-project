dcb637b55c07adfe8b5de20b0491862ceda25a71
Georgiana <georgianabrailoiu@gmail.com>
2020-12-24 16:07:40 +0200

    Initial commit

---
+++ mpai/src/main/java/eb/project/mpai/MpaiApplication.java
@@ -0,0 +1,13 @@
+package eb.project.mpai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpaiApplication.class, args);
+	}
+
+}
---
+++ mpai/src/test/java/eb/project/mpai/MpaiApplicationTests.java
@@ -0,0 +1,13 @@
+package eb.project.mpai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
