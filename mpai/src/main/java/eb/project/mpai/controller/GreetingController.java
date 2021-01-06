e6557ba0950abe54d60c03afe51ba6e7bbf06c82
Georgiana <georgianabrailoiu@gmail.com>
2021-01-06 19:35:48 +0200

    Added create account functionality

--- mpai/src/main/java/eb/project/mpai/controller/GreetingController.java
+++ mpai/src/main/java/eb/project/mpai/controller/GreetingController.java
@@ -1,17 +1,30 @@
 package eb.project.mpai.controller;
 
+import eb.project.mpai.domain.Utilizator;
+import eb.project.mpai.service.interfaces.UtilizatorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GreetingController {
 
+    @Autowired
+    private UtilizatorService utilizatorService;
+
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String greetingForm(Model model) {
+        model.addAttribute("greeting", new Utilizator());
         return "greeting";
     }
 
+    @PostMapping("/greeting")
+    public String greetingSubmit(@ModelAttribute Utilizator greeting, Model model) {
+        utilizatorService.addUtilizator(greeting);
+        model.addAttribute("greeting", greeting);
+        return "result";
+    }
 }
--- mpai/src/main/java/eb/project/mpai/service/interfaces/UtilizatorService.java
+++ mpai/src/main/java/eb/project/mpai/service/interfaces/UtilizatorService.java
@@ -1,12 +1,12 @@
 package eb.project.mpai.service.interfaces;
 
 import eb.project.mpai.domain.Utilizator;
-import eb.project.mpai.repository.UtilizatorRepository;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 public interface UtilizatorService {
 
     List<Utilizator> findAll();
+
+    Utilizator addUtilizator(Utilizator utilizator);
 }
