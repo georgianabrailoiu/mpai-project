050ceb7be8c27d335e6333d59a7a0e0c50e96c61
Anca <anca.bute@gmail.com>
2021-01-08 21:29:59 +0200

    Implemented builder design pattern

--- mpai/src/main/java/eb/project/mpai/domain/Utilizator.java
+++ mpai/src/main/java/eb/project/mpai/domain/Utilizator.java
@@ -24,8 +24,33 @@ public class Utilizator implements State  {
     /*  Mirela */
     private transient State stare;
 
+    @Override
+    public String toString() {
+        return "Nume: " + nume + "\n" +
+                "E-mail: " + email + "\n" +
+                "Parola: " + parola + "\n" +
+                "Numar de telefon: " + nrTel + "\n" +
+                "Adresa: " + adresa + "\n" +
+                "Cod postal: " + codPostal + "\n" +
+                "Varsta: " + varsta + "\n" +
+                "Stare civila: " + stareCivila + "\n";
+    }
 
+    // Anca
+    private Utilizator(BuilderUtilizator builder){
+        this.nume = builder.nume;
+        this.email = builder.email;
+        this.parola = builder.parola;
+        this.nrTel = builder.nrTel;
+        this.adresa = builder.adresa;
+        this.codPostal = builder.codPostal;
+        this.varsta = builder.varsta;
+        this.stareCivila = builder.stareCivila;
+    }
 
+    public Utilizator(){
+
+    }
 
     public String getNrTel() {
         return nrTel;
@@ -114,5 +139,48 @@ public class Utilizator implements State  {
         this.stare.doAction();
     }
 
+    public static class BuilderUtilizator {
+        //componente obligatorii
+        private String nume;
+        private String email;
+        private String parola;
+        private String nrTel;
+
+        //componente optionale
+        private String adresa;
+        private String codPostal;
+        private Integer varsta;
+        private String stareCivila;
+
+        public BuilderUtilizator(String nume, String email, String parola, String nrTel){
+            this.nume = nume;
+            this.email = email;
+            this.parola = parola;
+            this.nrTel = nrTel;
+        }
+
+        public BuilderUtilizator setAdresa(String adresa){
+            this.adresa = adresa;
+            return this;
+        }
+
+        public BuilderUtilizator setCodPostal (String codPostal){
+            this.codPostal = codPostal;
+            return this;
+        }
+
+        public BuilderUtilizator setVarsta(Integer varsta){
+            this.varsta = varsta;
+            return this;
+        }
+
+        public BuilderUtilizator setStareCivila(String stareCivila){
+            this.stareCivila = stareCivila;
+            return this;
+        }
+
+        public Utilizator build() {return new Utilizator(this);}
+    }
 
 }
+
